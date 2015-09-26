4eda1dc2f0a75510992e09a248d4aa59cc8165d8
Patrik Axelsson <patrik.bo@gmail.com>
2015-09-26 23:34:40 +0200

    Updated readme again

--- zip2gz.py
+++ zip2gz.py
@@ -2,8 +2,8 @@
 
 # This script takes a zip file as input and creates a tree of files containing
 # the same raw data as they have in the zip file, but in a way that is usable.
-# So for example if a file is compressed with deflate, it will take that raw
-# deflate data and slap a gzip header and footer around it.
+# So for example if a file in the zip file is compressed with deflate, it will
+# take that raw deflate data and slap a gzip header and footer around it.
 
 import zipfile
 import struct
